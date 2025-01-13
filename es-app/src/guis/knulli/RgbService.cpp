571cbdb999425f7f68454e66c7bfaad684d79662
chrizzo-hb <chrizzo-hb@null.void>
2025-01-13 02:03:17 +0100

    Existence check for RGB service calls

--- es-app/src/guis/knulli/RgbService.cpp
+++ es-app/src/guis/knulli/RgbService.cpp
@@ -5,18 +5,23 @@
 #include "utils/StringUtil.h"
 #include <stdio.h>
 #include <sys/wait.h>
+#include <fstream>
 
 const std::string RGB_SERVICE_NAME = "/usr/bin/analog_stick_led_daemon.sh";
 const std::string RGB_COMMAND_NAME = "/usr/bin/analog_stick_led.sh";
 
 void RgbService::start()
 {
-	system((RGB_SERVICE_NAME + " start").c_str());
+	if (Utils::FileSystem::exists(RGB_SERVICE_NAME)) {
+		system((RGB_SERVICE_NAME + " start").c_str());
+	}
 }
 
 void RgbService::stop()
 {
-	system((RGB_SERVICE_NAME + " stop").c_str());
+	if (Utils::FileSystem::exists(RGB_SERVICE_NAME)) {
+		system((RGB_SERVICE_NAME + " stop").c_str());
+	}
 }
 
 void RgbService::setRgb(int mode, int brightness, int speed, int r, int g, int b) {
