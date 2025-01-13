3a3d481b2baa925491e495fd73dff37dd4ae7849
chrizzo-hb <chrizzo-hb@null.void>
2025-01-14 00:25:58 +0100

    Option to restore default RGB colors, fixed service restart

--- es-app/src/guis/knulli/GuiRgbSettings.cpp
+++ es-app/src/guis/knulli/GuiRgbSettings.cpp
@@ -63,6 +63,7 @@ GuiRgbSettings::GuiRgbSettings(Window* window) : GuiSettings(window, _("RGB LED 
     sliderLedGreen->setValue(rgbValues[1]);
     sliderLedBlue = createSlider("BLUE", 0.f, 255.f, 1.f, "", "");
     sliderLedBlue->setValue(rgbValues[2]);
+    addEntry(_("RESTORE DEFAULT COLORS"), true, [this] { restoreDefaultColors(); });
 
     addGroup(_("BATTERY CHARGE INDICATION"));
 
@@ -158,7 +159,7 @@ std::array<float, 3> GuiRgbSettings::getRgbValues()
 {
     std::string colour = SystemConf::getInstance()->get("led.colour");
     if (colour.empty()) {
-        return {0, 0, 0};
+        return {DEFAULT_COLOR_RED, DEFAULT_COLOR_GREEN, DEFAULT_COLOR_BLUE};
     }
 
     std::vector<std::string> rgbValues;
@@ -202,4 +203,12 @@ void GuiRgbSettings::applyValues()
     int selectedGreen = (int) sliderLedGreen->getValue();
     int selectedBlue = (int) sliderLedBlue->getValue();
     RgbService::setRgb(std::stoi(selectedMode), selectedBrightness, selectedSpeed, selectedRed, selectedGreen, selectedBlue);
+}
+
+void GuiRgbSettings::restoreDefaultColors()
+{
+    sliderLedRed->setValue(DEFAULT_COLOR_RED);
+    sliderLedGreen->setValue(DEFAULT_COLOR_GREEN);
+    sliderLedBlue->setValue(DEFAULT_COLOR_BLUE);
+    applyValues();
 }
