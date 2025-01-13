5a5d1a9cde948d70024f15d09ade4a1dcb4fdb57
chrizzo-hb <chrizzo-hb@null.void>
2025-01-13 03:24:22 +0100

    Apply LED settings manually

--- es-app/src/guis/knulli/RgbService.cpp
+++ es-app/src/guis/knulli/RgbService.cpp
@@ -9,21 +9,51 @@
 
 const std::string RGB_SERVICE_NAME = "/usr/bin/analog_stick_led_daemon.sh";
 const std::string RGB_COMMAND_NAME = "/usr/bin/analog_stick_led.sh";
+const std::string SEPARATOR = "";
+const std::string START = "start";
+const std::string STOP = "stop";
 
 void RgbService::start()
 {
 	if (Utils::FileSystem::exists(RGB_SERVICE_NAME)) {
-		system((RGB_SERVICE_NAME + " start").c_str());
+		system((RGB_SERVICE_NAME + SEPARATOR + START).c_str());
 	}
 }
 
 void RgbService::stop()
 {
 	if (Utils::FileSystem::exists(RGB_SERVICE_NAME)) {
-		system((RGB_SERVICE_NAME + " stop").c_str());
+		system((RGB_SERVICE_NAME + SEPARATOR + STOP).c_str());
 	}
 }
 
+// TODO: This is a prototype. First improve the RGB bash scripts, then adopt the changes here.
 void RgbService::setRgb(int mode, int brightness, int speed, int r, int g, int b) {
+	
+	modeString = std::to_string(mode);
+	brightnessString = std::to_string(brightness);
+	speedString = std::to_string(speed);
+	rString = std::to_string(r);
+	gString = std::to_string(g);
+	bString = std::to_string(b);
+
+	if (mode == 0) {
+		system((RGB_COMMAND_NAME + SEPARATOR + modeString).c_str());
+	}
+	else if (mode < 5) {
+		system((RGB_COMMAND_NAME
+			 + SEPARATOR + modeString
+			 + SEPARATOR + brightnessString
+			 + SEPARATOR + rString
+			 + SEPARATOR + gString
+			 + SEPARATOR + bString
+			).c_str());
+	} else {
+		system((RGB_COMMAND_NAME
+			+ SEPARATOR + modeString
+			+ SEPARATOR + brightnessString
+			+ SEPARATOR + speedString
+			).c_str());
+	}
 
 }
